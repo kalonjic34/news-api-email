5560c2e19e22b89e5df090cd73ddd1d21850a1eb
Christian <kalonjic34@gmail.com>
2025-07-08 16:24:57 +0200

    Fetch and print news article titles and descriptions

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ api_key = "95c2ea503e8d4c5a80fb9036d9c0c4ea"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2025-06-08&sortBy=publishedAt&apiKey=95c2ea503e8d4c5a80fb9036d9c0c4ea"
 
 
+# make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# get a dictionary with data
+content = request.json()
+
+# access article title and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
