0282012f0898fc4afa39673dd49002859d796ecd
Christian <kalonjic34@gmail.com>
2025-07-08 17:24:17 +0200

    Send email with daily news headlines

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests 
+from send_email import send_email
 
 api_key = "95c2ea503e8d4c5a80fb9036d9c0c4ea"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2025-06-08&sortBy=publishedAt&apiKey=95c2ea503e8d4c5a80fb9036d9c0c4ea"
@@ -11,6 +12,10 @@ request = requests.get(url)
 content = request.json()
 
 # access article title and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:  
+        body = body + article["title"] +"\n"+ article["description"] + 2*"\n"
+        
+body = body.encode("utf-8")   
+send_email(message=body)
